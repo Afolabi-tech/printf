2ae785005034a30240de3f9c5303ae3a19abfddd
root <root@DESKTOP-TRN89PE.localdomain>
2026-07-08 08:26:18 +0100

    print_string - return and count strings

---
+++ print_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+//print_string - return strings
+
+int print_string(char *p_string){
+    int index = 0;
+
+    if (p_string == NULL) 
+        p_string = "(null)";
+   
+
+    while (p_string[index] != '\0') {
+
+        write(1, &p_string[index], 1);
+        index++;
+
+    }
+
+    return (index);
+
+
+  
+
+}
