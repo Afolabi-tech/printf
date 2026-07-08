5c536bc113710f6038d8ec8a9ff7afc64de82895
root <root@DESKTOP-TRN89PE.localdomain>
2026-07-08 08:28:55 +0100

    main.h - prototype headings

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+
+
+int _printf(const char *format, ...);
+int print_char(char p_char);
+int print_string(char *p_string);
+
+#endif
