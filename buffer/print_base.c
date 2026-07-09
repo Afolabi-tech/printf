4243c7effe3437454c5c1c53ff03c834175d450b
root <root@DESKTOP-TRN89PE.localdomain>
2026-07-09 16:37:30 +0100

    function - local buffer of 1024 chars in order to call write as little as possible

---
+++ buffer/print_base.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+int print_base(unsigned int n, 
+               char *base,
+               char buffer[],
+               int *buf_index)
+
+{
+    int count;
+    int base_len;
+    
+
+    count = 0;
+    base_len = 0;
+
+    while (base[base_len] != '\0')
+        base_len++;
+
+    if (n >= (unsigned int)base_len)
+        count += print_base(n / base_len, 
+                            base,
+                            buffer,
+                            buf_index);
+
+    count += add_to_buffer(base[n % base_len],
+                          buffer,
+                          buf_index);
+    
+
+    return (count);
+}
