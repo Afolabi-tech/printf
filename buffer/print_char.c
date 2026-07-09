df9101cbd427dfb913ce10326be01bd01ee6575a
root <root@DESKTOP-TRN89PE.localdomain>
2026-07-09 16:37:01 +0100

    function - local buffer of 1024 chars in order to call write as little as possible

---
+++ buffer/print_char.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*print_char - return characters
+*/
+
+
+int print_char(char p_char, 
+              char buffer[], 
+              int *buf_index)
+
+{
+
+    return (add_to_buffer(p_char, buffer, buf_index));
+
+}
