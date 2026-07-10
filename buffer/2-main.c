e56b0c62d130ed1eb9b5045d0ef68a488f0deb04
root <root@DESKTOP-TRN89PE.localdomain>
2026-07-10 17:30:16 +0100

    test file for handling flag characters +, space and #

---
+++ buffer/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+int main(void)
+{
+
+    _printf("%+d\n", 98);
+    _printf("% d\n", 98);
+    _printf("%#o\n", 98);
+    _printf("%#x\n", 98);
+    _printf("%#X\n", 98);
+
+    return 0;
+}
+
